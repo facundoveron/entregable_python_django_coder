49f8f1e87c777bf12a2487b7aa41666173a04719
facundoveron <veronariel98@hotmail.com>
2026-09-22 13:37:19 -0300

    agregando enrutado, vistas, y estilos para la aplicacion de post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return render(request,'posts/inicio.html')
+
+def acerca_de(request):
+    return render(request,'posts/acerca.html')
